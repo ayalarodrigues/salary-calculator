fe06e51e26b625768a95664fb88bdc1d085286c0
Ayala <ayalafreire7@gmail.com>
2023-11-21 12:26:43 -0300

    Adding logout button to the menu and limiting non-numeric characters in inputs.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-let newSalary; // Definindo a variável fora das funções para ter um escopo global
+let newSalary;
 
 function logout() {
     alert('You have been logged out.');
+    window.close(); // Esta linha fecha a janela
 }
 
 function calculateNewSalary() {
-    let salary = parseFloat(document.getElementById('salary').value);
+    let salaryInput = document.getElementById('salary');
+    let salaryMessage = document.getElementById('salaryMessage');
+
+    if (isNaN(salaryInput.value) || salaryInput.value === '') {
+        salaryMessage.textContent = 'Please enter a valid number.';
+        salaryMessage.style.color = 'red';
+        return;
+    }
+
+    let salary = parseFloat(salaryInput.value);
 
     if (salary <= 1200) {
         newSalary = salary * 1.15;
@@ -16,27 +26,26 @@ function calculateNewSalary() {
     }
 
     document.getElementsByClassName('result')[0].textContent = 'Your new salary is: $' + newSalary.toFixed(2);
+    salaryMessage.textContent = 'Only numbers are allowed.';
+    salaryMessage.style.color = '#777';
 }
 
 function vacation() {
-    calculateNewSalary(); // Calcular o novo salário
+    calculateNewSalary();
     let vacation = newSalary + (newSalary / 3);
 
     document.getElementsByClassName('result')[0].textContent = 'Your vacation is worth: $' + vacation.toFixed(2);
 }
 
 function thirteenth() {
-    calculateNewSalary(); // Calcular o novo salário
+    calculateNewSalary();
     let months = parseInt(document.getElementById('months').value);
 
     if (months > 12) {
-        // Mostrar mensagem de erro e não realizar o cálculo
         document.getElementsByClassName('result')[0].textContent = 'Please enter a maximum of 12 months.';
     } else if (months <= 0) {
-        // Mostrar mensagem de erro para meses inválidos
         document.getElementsByClassName('result')[0].textContent = 'Please enter a valid number of months.';
     } else {
-        // Calcular o décimo terceiro salário
         let thirteenth = newSalary * (months / 12);
         document.getElementsByClassName('result')[0].textContent = 'Your thirteenth is worth: $' + thirteenth.toFixed(2);
     }
